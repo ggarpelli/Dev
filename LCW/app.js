8bd48c6b43565f71190ca73fdc454037197be1e2
Guilherme Garpelli <ggarpelli@gmail.com>
2022-12-03 14:06:38 -0300

    Comentando no JS

--- LCW/app.js
+++ LCW/app.js
@@ -3,3 +3,13 @@ console.log("Bem Vindo ao Log");
 console.error("Eu sou um erro");
 
 console.warn("Aviso");
+
+//comentando do JS
+
+/* é igual no apex
+
+abre lá, e fecha aqui
+
+*/
+
+console.log("Aqui NÃO ESTÁ COMENTANDO MAIS");
